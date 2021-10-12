f62245ea4a236d00efde038a8742cac81812e729
AAnnan <62538347+AAnnan@users.noreply.github.com>
2021-10-12 10:56:49 +0200

    rectify rawcounts for 1-run SalmonGT

--- Build_Rawcounts_GT_EISA.sh
+++ Build_Rawcounts_GT_EISA.sh
@@ -11,11 +11,9 @@
 #SBATCH --job-name="file_prep_Salmon"
 
 ## Run in folder containing: 
-# EISA_23_09_2021_Tx, EISA_23_09_2021_Genes with Salmon input and outpt
+# EISA_23_09_2021_GT with Salmon input (multifastas) and outpt (salmon_out)
 
 # Variable names for file paths
-g="Genes"
-t="Tx"
 d="salmon_out"
 sample=("366" "382")
 rep=("b1" "b2" "b3" "b4")
@@ -24,8 +22,8 @@ rep=("b1" "b2" "b3" "b4")
 echo "Building Feature->GeneID correspondance table"
 
 # Extract the gene/transcript number and its corresponding gene ID
-grep '>' ${t}/c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa | sed 's/>//g' | sed 's/gene=//g' > gene_tx_list.txt
-grep '>' ${g}/CE_genes_seq.fa | sed 's/>//g' > gene_gen_list.txt
+grep '>' c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa | sed 's/>//g' | sed 's/gene=//g' > gene_tx_list.txt
+grep '>' CE_genes_seq.fa | sed 's/>//g' > gene_gen_list.txt
 
 
 ### Build read count columns by sample / replicate
@@ -33,16 +31,16 @@ for s in ${sample[@]}; do
 	for r in ${rep[@]}; do
 		echo "Extracting read count from "${s}${r}
 
-		echo ${s}${r} > NumReads_gen_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_gen_list.txt ${g}/${d}/${s}${r}/quant.sf >> NumReads_gen_${s}${r}.sf
-		echo ${s}${r} > NumReads_tx_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_tx_list.txt ${t}/${d}/${s}${r}/quant.sf >> NumReads_tx_${s}${r}.sf
+		echo ${s}${r} > NumReads_gen_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_gen_list.txt ${d}/${s}${r}/quant.sf >> NumReads_gen_${s}${r}.sf
+		echo ${s}${r} > NumReads_tx_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_tx_list.txt ${d}/${s}${r}/quant.sf >> NumReads_tx_${s}${r}.sf
 	done
 done
 
 ### Build final raw geneic/transcriptic counts
 echo "Building raw geneic/transcriptic count files"
 
-echo "Gene_IDs" > order_WBGenes_GE && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${g}/${d}/${s[0]}${r[0]}/quant.sf gene_gen_list.txt >> order_WBGenes_GE
-echo "Gene_IDs" > order_WBGenes_Tx && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${t}/${d}/${s[0]}${r[0]}/quant.sf gene_tx_list.txt >> order_WBGenes_Tx
+echo "Gene_IDs" > order_WBGenes_GE && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${d}/${s[0]}${r[0]}/quant.sf gene_gen_list.txt >> order_WBGenes_GE
+echo "Gene_IDs" > order_WBGenes_Tx && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${d}/${s[0]}${r[0]}/quant.sf gene_tx_list.txt >> order_WBGenes_Tx
 
 # Paste together all the columns
 paste order_WBGenes_Tx NumReads_tx_366b1.sf NumReads_tx_366b2.sf NumReads_tx_366b3.sf NumReads_tx_366b4.sf NumReads_tx_382b1.sf NumReads_tx_382b2.sf NumReads_tx_382b3.sf NumReads_tx_382b4.sf > rawcounts_transcript.txt
