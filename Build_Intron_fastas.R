dadf28001c7a20908f862569fc9bc47567d844f6
AAnnan <62538347+AAnnan@users.noreply.github.com>
2021-10-12 09:39:43 +0200

    remove CDSs (they're just exons)

--- Build_Intron_fastas.R
+++ Build_Intron_fastas.R
@@ -12,15 +12,12 @@ CE_gtf_txdb <- makeTxDbFromGFF("wormbase/c_elegans.PRJNA13758.WS279.annotations.
 
 #linked.to.single.gene.only
 #excludes (TRUE) or includes (FALSE) exons (or introns) not linked to a gene or linked to more than one gene.
-CE_cds <- cds(CE_gtf_txdb, columns=c("gene_id","tx_id", "tx_name"), filter=NULL)
+CE_exons <- exonicParts(CE_gtf_txdb, linked.to.single.gene.only=FALSE)
 CE_introns <- intronicParts(CE_gtf_txdb, linked.to.single.gene.only=FALSE)
 
 #Remove introns that overlap with any cds 
 #Also remove cds that overlap with any intron
-CE_introns_NO <- subsetByOverlaps(CE_introns, CE_cds, invert = TRUE, ignore.strand=FALSE)
-CE_cds_NO <- subsetByOverlaps(CE_cds, CE_introns, invert = TRUE, ignore.strand=FALSE)
-#Verify that no overlap is left
-#findOverlaps(CE_introns_NO, CE_cds_NO)
+CE_introns_NO <- subsetByOverlaps(CE_introns, CE_exons, invert = TRUE, ignore.strand=FALSE)
 
 # Trimming operations
 e=95L # length of the trim/expansion on either side of the feature
@@ -34,9 +31,9 @@ CE_introns_NO@ranges@width <- CE_introns_NO@ranges@width + e
 
 #Overlaid distributions of exon/intron length
 ggplot() + 
-  geom_histogram(data = data.frame(cds_sizes = log2(CE_cds_NO@ranges@width)), aes(x = cds_sizes, fill = "CDS"), binwidth=0.05, alpha = 0.5) +
+  geom_histogram(data = data.frame(exon_sizes = log2(CE_exons@ranges@width)), aes(x = exon_sizes, fill = "Exons"), binwidth=0.05, alpha = 0.5) +
   geom_histogram(data = data.frame(intron_sizes = log2(CE_introns_NO@ranges@width)), aes(x = intron_sizes, fill = "Introns"), binwidth=0.05, alpha = 0.5) +
-  scale_fill_manual(name ="", values = c("CDS" = "red", "Introns" = "blue")) + xlab("Length in log2(bp)")
+  scale_fill_manual(name ="", values = c("Exons" = "red", "Introns" = "blue")) + xlab("Length in log2(bp)")
 
 #### Make the Multi-fastas
 #Import ce11 genome
@@ -45,13 +42,22 @@ CE_genome <- BSgenome.Celegans.UCSC.ce11
 seqlevelsStyle(CE_genome) <- "Ensembl"
 
 #Retrieve sequence data
-CE_cds_seq <- getSeq(CE_genome,CE_cds_NO)
 CE_introns_seq <- getSeq(CE_genome,CE_introns_NO)
 
-#Add the descritpion line (gene name) to the DNAseq object
-CE_introns_seq@ranges@NAMES <- unlist(CE_introns_NO@elementMetadata$gene_id)
-CE_cds_seq@ranges@NAMES <- unlist(CE_cds_NO@elementMetadata$gene_id)
+#Build a short description line for the multifasta
+#Just exon number (artificial) and associated gene ID
+get_des <- function(int_ex_GR, int_or_ex) {
+  idx <- seq(from = 1, to = length(int_ex_GR), by = 1)
+  gene_ids <- unlist(int_ex_GR@elementMetadata$gene_id)
+  
+  Des <- paste0(int_or_ex, idx,"\t", gene_ids)
+  
+  return(Des)
+}
+
+#Can't use the gene names directly because Salmon doesn't take duplicates
+#CE_introns_seq@ranges@NAMES <- unlist(CE_introns_NO@elementMetadata$gene_id)
+CE_introns_seq@ranges@NAMES <- get_des(CE_introns_NO, int_or_ex="intron")
 
 #export multifastas
-writeXStringSet(CE_cds_seq, "CE_cds_seq.fa", format="fasta", append=FALSE, compress=FALSE)
 writeXStringSet(CE_introns_seq, "CE_introns_seq.fa", format="fasta", append=FALSE, compress=FALSE)
