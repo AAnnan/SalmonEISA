168f82655e1093637ee4f19e9c24d40243ef3c50
AAnnan <62538347+AAnnan@users.noreply.github.com>
2021-11-02 18:22:27 +0100

    change for Mus musculus genome

--- Build_genes_fasta.R
+++ Build_genes_fasta.R
@@ -4,23 +4,22 @@ setwd("~/Documents/Project_IZB/biodata/rnaseq/")
 library(GenomicRanges)
 library(GenomicFeatures)
 library(rtracklayer)
-library(BSgenome.Celegans.UCSC.ce11)
+library(BSgenome.Mmusculus.UCSC.mm10)
 
-#CE_gtf_txdb <- makeTxDbFromGFF("c_elegans.PRJNA13758.WS281.annotations.gff3",format="gff3")
-CE_gtf_txdb <- makeTxDbFromGFF("wormbase/c_elegans.PRJNA13758.WS279.annotations.gff3",format="gff3")
+MM_gtf_txdb <- makeTxDbFromGFF("mm_ensembl/Mus_musculus.GRCm38.102.gff3",format="gff3")
 
-CE_genes <- genes(CE_gtf_txdb, columns="gene_id", filter=NULL, single.strand.genes.only=TRUE)
+MM_genes <- genes(MM_gtf_txdb, columns="gene_id", filter=NULL, single.strand.genes.only=TRUE)
 
 #### Make the Multi-fastas
 #Import ce11 genome
-CE_genome <- BSgenome.Celegans.UCSC.ce11
-seqlevelsStyle(CE_genome) <- "Ensembl" #ensembl style
+MM_genome <- BSgenome.Mmusculus.UCSC.mm10
+seqlevelsStyle(MM_genome) <- "ensembl" #ensembl style
 
 #Retrieve sequence data
-CE_genes_seq <- getSeq(CE_genome,CE_genes)
+MM_genes_seq <- getSeq(MM_genome,MM_genes)
 
 #Add the descritpion line to the DNAseq object
-CE_genes_seq@ranges@NAMES <- CE_genes@elementMetadata$gene_id
+MM_genes_seq@ranges@NAMES <- MM_genes@elementMetadata$gene_id
 
 #export Genes multifasta
-writeXStringSet(CE_genes_seq, "CE_genes_seq.fa", format="fasta", append=FALSE, compress=FALSE)
+writeXStringSet(MM_genes_seq, "MM_genes_seq.fa", format="fasta", append=FALSE, compress=FALSE)
