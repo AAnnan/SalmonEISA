750b0dfe6e34ea1a30b3e76093414b4d4da78d17
AAnnan <62538347+AAnnan@users.noreply.github.com>
2021-11-08 16:37:00 +0100

    Complete revamp of SalmonEISA

--- Build_Rawcounts_GT_EISA.sh
+++ Build_Rawcounts_GT_EISA.sh
@@ -11,42 +11,58 @@
 #SBATCH --job-name="file_prep_Salmon"
 
 ## Run in folder containing: 
-# EISA_23_09_2021_GT with Salmon input (multifastas) and outpt (salmon_out)
+# Salmon input (multifastas) and output (salmon_out)
 
 # Variable names for file paths
-d="salmon_out"
-sample=("366" "382")
-rep=("b1" "b2" "b3" "b4")
+d="../salmon_out"
+repb1=("1" "2" "3" "4")
+
+r366b=()
+r382b=()
+for r in ${repb1[@]}; do
+	r366b+="366b${r} "
+	r382b+="382b${r} "
+done
 
 ### Build Feature-Gene correspondance table
 echo "Building Feature->GeneID correspondance table"
 
+mkdir tmp
+cd tmp
 # Extract the gene/transcript number and its corresponding gene ID
-grep '>' c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa | sed 's/>//g' | sed 's/gene=//g' > gene_tx_list.txt
-grep '>' CE_genes_seq.fa | sed 's/>//g' > gene_gen_list.txt
+grep '>' ../c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa | sed 's/>//g' | sed 's/gene=//g' > gene_tx_list.txt
+grep '>' ../CE_genes_seq.fa | sed 's/>//g' > gene_gen_list.txt
 
+echo "Gene_IDs" > order_WBGenes_GE && awk 'NR==FNR{a[$1];next} $1 in a{print $1}' ${d}/366C1/quant.sf gene_gen_list.txt >> order_WBGenes_GE
+echo "Gene_IDs" > order_WBGenes_Tx && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${d}/366C1/quant.sf gene_tx_list.txt >> order_WBGenes_Tx
+
+mkdir gen
+mkdir tx
 
 ### Build read count columns by sample / replicate
-for s in ${sample[@]}; do
-	for r in ${rep[@]}; do
-		echo "Extracting read count from "${s}${r}
+for s in ${r366b[@]} ${r382b[@]} ; do
+	echo "Extracting read count from "${s}
 
-		echo ${s}${r} > NumReads_gen_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_gen_list.txt ${d}/${s}${r}/quant.sf >> NumReads_gen_${s}${r}.sf
-		echo ${s}${r} > NumReads_tx_${s}${r}.sf && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_tx_list.txt ${d}/${s}${r}/quant.sf >> NumReads_tx_${s}${r}.sf
-	done
+	echo ${s} > gen/${s} && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_gen_list.txt ${d}/${s}/quant.sf >> gen/${s}
+	echo ${s} > tx/${s} && awk 'NR==FNR{a[$1];next} $1 in a{print $5}' gene_tx_list.txt ${d}/${s}/quant.sf >> tx/${s}
 done
 
 ### Build final raw geneic/transcriptic counts
 echo "Building raw geneic/transcriptic count files"
 
-echo "Gene_IDs" > order_WBGenes_GE && awk 'NR==FNR{a[$1];next} $1 in a{print $1}' ${d}/${s[0]}${r[0]}/quant.sf gene_gen_list.txt >> order_WBGenes_GE
-echo "Gene_IDs" > order_WBGenes_Tx && awk 'NR==FNR{a[$1];next} $1 in a{print $2}' ${d}/${s[0]}${r[0]}/quant.sf gene_tx_list.txt >> order_WBGenes_Tx
+# Contrasts: 
+#WT2020_vs_dpy26cs2020
+#${r366b[@]} ${r382b[@]}
 
 # Paste together all the columns
-paste order_WBGenes_Tx NumReads_tx_366b1.sf NumReads_tx_366b2.sf NumReads_tx_366b3.sf NumReads_tx_366b4.sf NumReads_tx_382b1.sf NumReads_tx_382b2.sf NumReads_tx_382b3.sf NumReads_tx_382b4.sf > rawcounts_transcript.txt
-paste order_WBGenes_GE NumReads_gen_366b1.sf NumReads_gen_366b2.sf NumReads_gen_366b3.sf NumReads_gen_366b4.sf NumReads_gen_382b1.sf NumReads_gen_382b2.sf NumReads_gen_382b3.sf NumReads_gen_382b4.sf > rawcounts_gene.txt
+cd gen
+paste ../order_WBGenes_GE ${r366b[@]} ${r382b[@]} > ../order_WT2020_vs_dpy26cs2020_rawcounts_gene.txt
+
+cd ../tx
+paste ../order_WBGenes_Tx ${r366b[@]} ${r382b[@]} > ../WT2020_vs_dpy26cs2020_rawcounts_transcript.tx
 
 # Clean up
-rm g*_t*_list.txt g*_g*_list.txt
-rm NumReads*.sf
+mv *raw* ..
+cd ..
 rm order*
+rm -r tmp
--- Build_genes_fasta.R
+++ Build_genes_fasta.R
@@ -3,12 +3,12 @@ setwd("~/Documents/Project_IZB/biodata/rnaseq/")
 
 library(GenomicRanges)
 library(GenomicFeatures)
-library(rtracklayer)
 library(BSgenome.Celegans.UCSC.ce11)
 
-#CE_gtf_txdb <- makeTxDbFromGFF("c_elegans.PRJNA13758.WS281.annotations.gff3",format="gff3")
+# Make a TxDb GFF3 WS annotation
 CE_gtf_txdb <- makeTxDbFromGFF("wormbase/c_elegans.PRJNA13758.WS279.annotations.gff3",format="gff3")
 
+# Extract unspliced transcripts (=genes)
 CE_genes <- genes(CE_gtf_txdb, columns="gene_id", filter=NULL, single.strand.genes.only=TRUE)
 
 #### Make the Multi-fastas
@@ -20,7 +20,7 @@ seqlevelsStyle(CE_genome) <- "Ensembl" #ensembl style
 CE_genes_seq <- getSeq(CE_genome,CE_genes)
 
 #Add the descritpion line to the DNAseq object
-CE_genes_seq@ranges@NAMES <- CE_genes@elementMetadata$gene_id
+CE_genes_seq@ranges@NAMES <- gsub("Gene:", "", CE_genes_seq@ranges@NAMES)
 
 #export Genes multifasta
 writeXStringSet(CE_genes_seq, "CE_genes_seq.fa", format="fasta", append=FALSE, compress=FALSE)
