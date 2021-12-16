48ab47f5a8ca0010556740144693f90ee07c6e0a
AAnnan <62538347+AAnnan@users.noreply.github.com>
2021-12-16 17:34:07 +0100

    launch STAR + 2 SALMON

--- Run_SalmonRNASEQ.sh
+++ Run_SalmonRNASEQ.sh
@@ -3,8 +3,8 @@
 ## Resource Allocation
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=gpu
-#SBATCH --mem=320G
-#SBATCH –-ntasks=64
+#SBATCH --mem=340G
+#SBATCH –-ntasks=72
 
 #SBATCH --mail-user=ahrmad.annan@students.unibe.ch
 #SBATCH --mail-type=end,fail
@@ -76,53 +76,53 @@ faTOP2p_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_3_1.fq.gz
 faTOP2p_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_3_2.fq.gz
 
 
-cat c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa CE_genes_seq.fa c_elegans.PRJNA13758.WS279.genomic.fa > CE_gentrome.fa
+cat c_elegans.PRJNA13758.WS279.mRNA_transcripts.fa c_elegans.PRJNA13758.WS279.genomic.fa > CE_gentromeRNASEQ.fa
 
-salmon index -p 64 -t CE_gentrome.fa -i EISA_index --decoys decoys.txt -k 17
+salmon index -p 72 -t CE_gentromeRNASEQ.fa -i RNASEQ_index --decoys decoys.txt -k 17
 #the k size selected here will act as the minimum acceptable length for a valid match.
 
 #AMA
-salmon quant -i EISA_index -l A -1 ${faAMA_1_1} -2 ${faAMA_1_2} --validateMappings -p 64 -o salmon_out/AMA_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faAMA_2_1} -2 ${faAMA_2_2} --validateMappings -p 64 -o salmon_out/AMA_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faAMA_1_1} -2 ${faAMA_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/AMA_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faAMA_2_1} -2 ${faAMA_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/AMA_2 --seqBias --gcBias 
 
 #FLAVO
-salmon quant -i EISA_index -l A -1 ${faFLAVO_1_1} -2 ${faFLAVO_1_2} --validateMappings -p 64 -o salmon_out/FLAVO_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faFLAVO_2_1} -2 ${faFLAVO_2_2} --validateMappings -p 64 -o salmon_out/FLAVO_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faFLAVO_1_1} -2 ${faFLAVO_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/FLAVO_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faFLAVO_2_1} -2 ${faFLAVO_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/FLAVO_2 --seqBias --gcBias 
 
 #N2
-salmon quant -i EISA_index -l A -1 ${faN2_1_1} -2 ${faN2_1_2} --validateMappings -p 64 -o salmon_out/N2_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faN2_2_1} -2 ${faN2_2_2} --validateMappings -p 64 -o salmon_out/N2_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faN2_1_1} -2 ${faN2_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/N2_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faN2_2_1} -2 ${faN2_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/N2_2 --seqBias --gcBias 
 
 #TIR1m
-salmon quant -i EISA_index -l A -1 ${faTIR1m_1_1} -2 ${faTIR1m_1_2} --validateMappings -p 64 -o salmon_out/TIR1m_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTIR1m_2_1} -2 ${faTIR1m_2_2} --validateMappings -p 64 -o salmon_out/TIR1m_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTIR1m_3_1} -2 ${faTIR1m_3_2} --validateMappings -p 64 -o salmon_out/TIR1m_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1m_1_1} -2 ${faTIR1m_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1m_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1m_2_1} -2 ${faTIR1m_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1m_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1m_3_1} -2 ${faTIR1m_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1m_3 --seqBias --gcBias 
 
 #TIR1p
-salmon quant -i EISA_index -l A -1 ${faTIR1p_1_1} -2 ${faTIR1p_1_2} --validateMappings -p 64 -o salmon_out/TIR1p_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTIR1p_2_1} -2 ${faTIR1p_2_2} --validateMappings -p 64 -o salmon_out/TIR1p_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTIR1p_3_1} -2 ${faTIR1p_3_2} --validateMappings -p 64 -o salmon_out/TIR1p_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1p_1_1} -2 ${faTIR1p_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1p_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1p_2_1} -2 ${faTIR1p_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1p_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTIR1p_3_1} -2 ${faTIR1p_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TIR1p_3 --seqBias --gcBias 
 
 #TOP1m
-salmon quant -i EISA_index -l A -1 ${faTOP1m_1_1} -2 ${faTOP1m_1_2} --validateMappings -p 64 -o salmon_out/TOP1m_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP1m_2_1} -2 ${faTOP1m_2_2} --validateMappings -p 64 -o salmon_out/TOP1m_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP1m_3_1} -2 ${faTOP1m_3_2} --validateMappings -p 64 -o salmon_out/TOP1m_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1m_1_1} -2 ${faTOP1m_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1m_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1m_2_1} -2 ${faTOP1m_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1m_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1m_3_1} -2 ${faTOP1m_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1m_3 --seqBias --gcBias 
 
 #TOP1p
-salmon quant -i EISA_index -l A -1 ${faTOP1p_1_1} -2 ${faTOP1p_1_2} --validateMappings -p 64 -o salmon_out/TOP1p_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP1p_2_1} -2 ${faTOP1p_2_2} --validateMappings -p 64 -o salmon_out/TOP1p_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP1p_3_1} -2 ${faTOP1p_3_2} --validateMappings -p 64 -o salmon_out/TOP1p_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1p_1_1} -2 ${faTOP1p_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1p_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1p_2_1} -2 ${faTOP1p_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1p_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP1p_3_1} -2 ${faTOP1p_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP1p_3 --seqBias --gcBias 
 
 #TOP2m
-salmon quant -i EISA_index -l A -1 ${faTOP2m_1_1} -2 ${faTOP2m_1_2} --validateMappings -p 64 -o salmon_out/TOP2m_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP2m_2_1} -2 ${faTOP2m_2_2} --validateMappings -p 64 -o salmon_out/TOP2m_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP2m_3_1} -2 ${faTOP2m_3_2} --validateMappings -p 64 -o salmon_out/TOP2m_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2m_1_1} -2 ${faTOP2m_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2m_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2m_2_1} -2 ${faTOP2m_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2m_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2m_3_1} -2 ${faTOP2m_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2m_3 --seqBias --gcBias 
 
 #TOP2p
-salmon quant -i EISA_index -l A -1 ${faTOP2p_1_1} -2 ${faTOP2p_1_2} --validateMappings -p 64 -o salmon_out/TOP2p_1 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP2p_2_1} -2 ${faTOP2p_2_2} --validateMappings -p 64 -o salmon_out/TOP2p_2 --seqBias --gcBias 
-salmon quant -i EISA_index -l A -1 ${faTOP2p_3_1} -2 ${faTOP2p_3_2} --validateMappings -p 64 -o salmon_out/TOP2p_3 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2p_1_1} -2 ${faTOP2p_1_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2p_1 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2p_2_1} -2 ${faTOP2p_2_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2p_2 --seqBias --gcBias 
+salmon quant -i RNASEQ_index -l A -1 ${faTOP2p_3_1} -2 ${faTOP2p_3_2} --validateMappings -p 72 -o salmon_out_RNASEQ/TOP2p_3 --seqBias --gcBias 
 
 conda deactivate
 
-#sbatch --cpus-per-task 64 SALM
+#sbatch --cpus-per-task 72 SALM
---
+++ STARalign.sh
@@ -0,0 +1,244 @@
+#! /bin/bash
+
+## Resource Allocation
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=gpu
+#SBATCH --mem=320G
+#SBATCH –-cpus-per-task=96
+
+#SBATCH --mail-user=ahrmad.annan@students.unibe.ch
+#SBATCH --mail-type=end,fail
+#SBATCH --job-name="STAR"
+
+source ${CONDA_ACTIVATE} salmon
+
+/home/aannan/soft/gffread/gffread /mnt/external.data/aannan/EISA/c_elegans.PRJNA13758.WS279.annotations.gff3  -T -o c_elegans.PRJNA13758.WS279.annotations.gtf
+
+faAMA_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/AMA_1_1.fq.gz
+faAMA_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/AMA_1_2.fq.gz
+faAMA_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/AMA_2_1.fq.gz
+faAMA_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/AMA_2_2.fq.gz
+
+faFLAVO_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/FLAVO_1_1.fq.gz
+faFLAVO_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/FLAVO_1_2.fq.gz
+faFLAVO_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/FLAVO_2_1.fq.gz
+faFLAVO_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/FLAVO_2_2.fq.gz
+
+faN2_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/N2_1_1.fq.gz
+faN2_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/N2_1_2.fq.gz
+faN2_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/N2_2_1.fq.gz
+faN2_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/N2_2_2.fq.gz
+
+faTIR1m_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_1_1.fq.gz
+faTIR1m_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_1_2.fq.gz
+faTIR1m_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_2_1.fq.gz
+faTIR1m_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_2_2.fq.gz
+faTIR1m_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_3_1.fq.gz
+faTIR1m_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_m_3_2.fq.gz
+
+faTIR1p_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_1_1.fq.gz
+faTIR1p_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_1_2.fq.gz
+faTIR1p_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_2_1.fq.gz
+faTIR1p_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_2_2.fq.gz
+faTIR1p_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_3_1.fq.gz
+faTIR1p_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TIR1_p_3_2.fq.gz
+
+faTOP1m_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_1_1.fq.gz
+faTOP1m_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_1_2.fq.gz
+faTOP1m_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_2_1.fq.gz
+faTOP1m_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_2_2.fq.gz
+faTOP1m_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_3_1.fq.gz
+faTOP1m_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_m_3_2.fq.gz
+
+faTOP1p_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_1_1.fq.gz
+faTOP1p_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_1_2.fq.gz
+faTOP1p_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_2_1.fq.gz
+faTOP1p_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_2_2.fq.gz
+faTOP1p_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_3_1.fq.gz
+faTOP1p_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP1_p_3_2.fq.gz
+
+faTOP2m_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_1_1.fq.gz
+faTOP2m_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_1_2.fq.gz
+faTOP2m_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_2_1.fq.gz
+faTOP2m_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_2_2.fq.gz
+faTOP2m_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_3_1.fq.gz
+faTOP2m_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_m_3_2.fq.gz
+
+faTOP2p_1_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_1_1.fq.gz
+faTOP2p_1_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_1_2.fq.gz
+faTOP2p_2_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_2_1.fq.gz
+faTOP2p_2_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_2_2.fq.gz
+faTOP2p_3_1=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_3_1.fq.gz
+faTOP2p_3_2=/scratch/aannan/16_12_eisa_bolaji/Bolaji20211216/TOP2_p_3_2.fq.gz
+
+STAR --runMode genomeGenerate \
+--genomeDir ./sequence --genomeFastaFiles c_elegans.PRJNA13758.WS279.genomic.fa \
+--sjdbGTFfile c_elegans.PRJNA13758.WS281.annotations.gtf --runThreadN 96 \
+--genomeSAindexNbases 12 --sjdbOverhang 49 
+
+###########################################################################
+###########################################################################
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faAMA_1_1} ${faAMA_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/AMA_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faAMA_2_1} ${faAMA_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/AMA_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+###########################################################################
+###########################################################################
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faFLAVO_1_1} ${faFLAVO_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/FLAVO_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faFLAVO_2_1} ${faFLAVO_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/FLAVO_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+###########################################################################
+###########################################################################
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faN2_1_1} ${faN2_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/N2_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faN2_2_1} ${faN2_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/N2_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+###########################################################################
+###########################################################################
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1m_1_1} ${faTIR1m_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1m_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1m_2_1} ${faTIR1m_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1m_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1m_3_1} ${faTIR1m_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1m_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1p_1_1} ${faTIR1p_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1p_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1p_2_1} ${faTIR1p_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1p_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTIR1p_3_1} ${faTIR1p_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TIR1p_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+###########################################################################
+###########################################################################
+
+ STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1m_1_1} ${faTOP1m_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1m_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1m_2_1} ${faTOP1m_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1m_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1m_3_1} ${faTOP1m_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1m_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1p_1_1} ${faTOP1p_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1p_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1p_2_1} ${faTOP1p_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1p_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP1p_3_1} ${faTOP1p_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP1p_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+###########################################################################
+###########################################################################
+
+ STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2m_1_1} ${faTOP2m_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2m_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2m_2_1} ${faTOP2m_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2m_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2m_3_1} ${faTOP2m_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2m_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2p_1_1} ${faTOP2p_1_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2p_1_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2p_2_1} ${faTOP2p_2_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2p_2_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+STAR --genomeDir ./sequence \
+ --readFilesIn ${faTOP2p_3_1} ${faTOP2p_3_2} --readFilesCommand zcat \
+ --outFileNamePrefix ./bamSTAR/TOP2p_3_ \
+ --runThreadN 96 --outSAMmultNmax 1 \
+ --outSAMtype BAM SortedByCoordinate --outMultimapperOrder Random --alignIntronMax 5000 --quantMode GeneCounts --outWigType wiggle --outWigNorm RPM
+
+conda deactivate
+
+#sbatch --dependency=afterany: --cpus-per-task 96 STAR
+
