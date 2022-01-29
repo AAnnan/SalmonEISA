e07f4b4aca62ddc1d6532e2a25acc71ea9168ea1
AAnnan <62538347+AAnnan@users.noreply.github.com>
2022-01-29 10:41:04 +0100

    update

--- Build_genes_fasta.R
+++ Build_genes_fasta.R
@@ -1,12 +1,13 @@
 getwd()
 setwd("~/Documents/Project_IZB/biodata/rnaseq/")
+annotationFile <- "wormbase/c_elegans.PRJNA13758.WS279.annotations.gff3"
 
 library(GenomicRanges)
 library(GenomicFeatures)
 library(BSgenome.Celegans.UCSC.ce11)
 
 # Make a TxDb GFF3 WS annotation
-CE_gtf_txdb <- makeTxDbFromGFF("wormbase/c_elegans.PRJNA13758.WS279.annotations.gff3",format="gff3")
+CE_gtf_txdb <- makeTxDbFromGFF(annotationFile,format="gff3")
 
 # Extract unspliced transcripts (=genes)
 CE_genes <- genes(CE_gtf_txdb, columns="gene_id", filter=NULL, single.strand.genes.only=TRUE)
